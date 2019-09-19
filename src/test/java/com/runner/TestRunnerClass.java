07a3b42d8d0b42ed95344964b25156c37a449412
rajakumaran25 <rajakumaran.rs25@gmail.com>
2019-09-19 13:38:19 +0530

    testing

--- src/test/java/com/runner/TestRunnerClass.java
+++ src/test/java/com/runner/TestRunnerClass.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= {"src\\test\\resources\\Features"},glue= {"com.stepd"})
+@CucumberOptions(features= {"src\\test\\resources\\Features"},glue= {"com.stepd"},plugin= {"html:target1"})
 public class TestRunnerClass {
 
 }
